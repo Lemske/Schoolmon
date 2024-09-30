4f315a033229ecbe91af4f5efdee1a8673fa5326
Simon S.C <95408442+Lemske@users.noreply.github.com>
2024-09-30 12:43:39 +0200

    Initial commit

---
+++ Assets/Scripts/ImageTrackingListener.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTrackingListener : MonoBehaviour
+{
+    [SerializeField] ARTrackedImageManager imgTracker;
+
+    void OnEnable() => imgTracker.trackablesChanged.AddListener(OnChanged);
+    void OnDisable() => imgTracker.trackablesChanged.RemoveListener(OnChanged);
+
+    void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> evtArgs)
+    {
+        foreach (var newImage in evtArgs.added)
+        {
+            Debug.Log($"Image added: {newImage.referenceImage.name}");
+        }
+
+        foreach (var updatedImage in evtArgs.updated)
+        {
+            Debug.Log($"Image updated: {updatedImage.referenceImage.name}");
+        }
+
+        foreach (var removedImage in evtArgs.removed)
+        {
+            Debug.Log($"Image removed: {removedImage.Key}");
+        }
+    }
+}
