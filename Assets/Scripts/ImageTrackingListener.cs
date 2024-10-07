716cd30b31281cb6fa6fa63b6185d12011cef617
Simon S.C <95408442+Lemske@users.noreply.github.com>
2024-10-07 20:34:43 +0200

    Refactor and merethe model
    Placed the code a more correct way, and made it more flex. Tried getting the bug model into unity

--- Assets/Scripts/ImageTrackingListener.cs
+++ Assets/Scripts/ImageTrackingListener.cs
@@ -8,76 +8,31 @@ public class ImageTrackingListener : MonoBehaviour
 {
     [SerializeField] ARTrackedImageManager imgTracker;
     [SerializeField] XRReferenceImageLibrary library; //TODO: This should be done in some testing script
-    [SerializeField] GameObject[] prefabs;
-    private List<GameObject> instantiatedPrefabs;
+    [SerializeField] MonsterManager monsterManager;
     void OnEnable() => imgTracker.trackablesChanged.AddListener(OnChanged);
     void OnDisable() => imgTracker.trackablesChanged.RemoveListener(OnChanged);
 
     void Start()
     {
-        instantiatedPrefabs = new List<GameObject>();
-        Debug.Log("ImageTrackingListener Start");
-        //TODO: should be done in some testing script
-        HashSet<string> prefabNames = new HashSet<string>();
-        foreach (var prefab in prefabs)
+        //TODO: This should be done in some testing script
+        List<string> imgNames = new List<string>();
+        foreach (var img in library)
         {
-            prefabNames.Add(prefab.name);
+            imgNames.Add(img.name);
         }
-        ArrayList missingTrackables = new ArrayList();
-        foreach (var trackedImage in library)
-        {
-            name = trackedImage.name;
-            if (prefabNames.Contains(name))
-            {
-                prefabNames.Remove(name);
-            }
-            else
-            {
-                missingTrackables.Add("Missing Prefab: " + name);
-            }
-        }
-        foreach (string prefabName in prefabNames)
-        {
-            missingTrackables.Add("Missing Image: " + prefabName);
-        }
-        if (missingTrackables.Count > 0)
-        {
-            throw new System.Exception("\n" + string.Join("\n", missingTrackables.ToArray()));
-        }
-        //TODO: should be done in some testing script
+        monsterManager.CheckIfMissing(imgNames);
     }
 
     void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> evtArgs)
     {
         foreach (var newImage in evtArgs.added)
         {
-            //Debug.Log($"Image added: {newImage.referenceImage.name}");
-            foreach (var prefab in prefabs)
-            {
-                if (newImage.referenceImage.name == prefab.name)
-                {
-                    GameObject instantiated = Instantiate(prefab, newImage.transform.position, newImage.transform.rotation);
-                    Debug.Log($"Instantiated: {instantiated.name}");
-                    instantiatedPrefabs.Add(instantiated);
-                }
-            }
+            monsterManager.InstantiateMonster(newImage.referenceImage.name, newImage.transform.position, newImage.transform.rotation);
         }
-
         foreach (var updatedImage in evtArgs.updated)
         {
-            //Debug.Log($"Image updated: {updatedImage.referenceImage.name}");
-            foreach (var instantiatedPrefab in instantiatedPrefabs)
-            {
-                Debug.Log($"Instantiated: {instantiatedPrefab.name} Updated: {updatedImage.referenceImage.name}");
-                if (instantiatedPrefab.name.Contains(updatedImage.referenceImage.name))
-                {
-                    Debug.Log($"Updating: {instantiatedPrefab.name}");
-                    instantiatedPrefab.transform.position = updatedImage.transform.position;
-                }
-            }
-
+            monsterManager.UpdateMonsterPosition(updatedImage.referenceImage.name, updatedImage.transform.position, updatedImage.transform.rotation);
         }
-
         foreach (var removedImage in evtArgs.removed)
         {
             Debug.Log($"Image removed: {removedImage.Key}");
---
+++ Assets/Scripts/MonsterManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+    [SerializeField] private MonsterInstance[] monsters;
+    public void InstantiateMonster(string imgName, Vector3 position, Quaternion rotation)
+    {
+        MonsterInstance monster = FindMonsterFromImage(imgName);
+        if (monster != null)
+        {
+            GameObject monsterInstance = Instantiate(monster.prefab, position, rotation);
+            monster.Instance = monsterInstance;
+        }
+    }
+
+    public void UpdateMonsterPosition(string imgName, Vector3 position, Quaternion rotation)
+    {
+        MonsterInstance monster = FindMonsterFromImage(imgName);
+        if (monster != null)
+        {
+            monster.Instance.transform.position = position;
+        }
+    }
+
+    private MonsterInstance FindMonsterFromImage(string imgName)
+    {
+        foreach (var monster in monsters)
+        {
+            if (imgName == monster.trackedImage.name)
+            {
+                return monster;
+            }
+        }
+        throw new System.Exception("Missing Image in Manager: " + imgName + "\nCheck the testing for failures");
+    }
+
+    public void CheckIfMissing(List<string> imgNames)
+    {
+        List<string> missingImages = new List<string>();
+        foreach (var monster in monsters)
+        {
+            if (imgNames.Contains(monster.trackedImage.name))
+            {
+                imgNames.Remove(monster.trackedImage.name);
+            }
+            else
+            {
+                Debug.Log("Missing Image from Tracking: " + monster.trackedImage.name);
+            }
+        }
+        if (missingImages.Count > 0)
+        {
+            throw new System.Exception("Missing Images in Manager: " + string.Join(", ", missingImages.ToArray()));
+        }
+    }
+
+    [System.Serializable]
+    public class MonsterInstance
+    {
+        [SerializeField] public GameObject prefab;
+        [SerializeField] public Texture2D trackedImage;
+        private GameObject instance;
+
+        public GameObject Instance
+        {
+            get => instance;
+            set => instance = value;
+        }
+    }
+}
